69f3cd95f4b6ac5ef456c78d27b8a084c2b854ca
matrix <usergit436@gmail.com>
2022-01-03 17:06:07 +0630

    rebuild project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 function buildStyles() {
-  return src('index.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+  return src("Scss/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(["Scss/**/*.scss"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
